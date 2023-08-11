d774d173170b1e9749c0800881ca3d6c20f4199b
amrazp <amrazp1233@gmail.com>
2023-08-11 18:11:21 +0300

    123

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char nm  = " \"Programming is like building a multilingual puzzle " ;
+	puts(nm);
+	return(0);
+}
--- 4-puts.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	char nm = " \"Programming is like building a multilingual puzzle " ;
-	puts(nm);
-	return(0);
-}
