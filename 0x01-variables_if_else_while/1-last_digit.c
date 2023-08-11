2a67f4cc311d898d474484d6cc4dbae9216865f7
amrazp <amrazp1233@gmail.com>
2023-08-12 01:33:18 +0300

    12

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
-/**
-* main - Entry point
-* void - empty
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
-	char c;
-
-	for  (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+    char ch;
+    for (ch = 'a' ; ch <= 'z'; ch++)
+    {
+        printf("%c", ch);
+    }
+    return 0;
 }
