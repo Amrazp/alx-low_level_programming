0d4a8b5889b3fa1dc59229209dd73ab42c30e119
amrazp <amrazp1233@gmail.com>
2023-08-12 03:10:59 +0300

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-
-int main(void)
+int main (void)
 {
-    int a;
-    for ( a = 0; a < 10; a++)
-    {
-        printf("%d", a);
-    }
-    return(0);
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+return (0);
 }
