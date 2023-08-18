3b14481b6a81a22de4cbfb86e65a81415356cf2a
amrazp <amrazp1233@gmail.com>
2023-08-18 10:17:12 +0300

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - entry point
+ * print_most_numbers - entry point
  *
  * Description: Prints the numbers with _putchar
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	_putchar("0123456789");
-	
+	_putchar("01356789");
 	_putchar('\n');
 }
