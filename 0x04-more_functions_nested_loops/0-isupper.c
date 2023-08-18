23ea2a81654cb7e0fd78d64a174771ae402d175c
amrazp <amrazp1233@gmail.com>
2023-08-18 09:22:38 +0300

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - input
+ *
+ * Description: define if a character is a number
+ * @c: charater
+ * Return: 1 yes 0 no
+ */ 
+
+int _isupper(int c)
+{
+    if ((c >= 'A') && (c <= 'z'))
+    
+        return(1);
+    else 
+        return(0);
+}
