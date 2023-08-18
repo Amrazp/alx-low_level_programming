040abb083844e56de85030cb44480e762d75f290
amrazp <amrazp1233@gmail.com>
2023-08-18 10:57:08 +0300

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: Prints lines
+ *@n: number of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (n = 1; n <= n; n++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
