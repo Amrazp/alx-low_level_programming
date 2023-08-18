d3f6d7f84b6a22348a00f7335f09f041e50aec18
amrazp <amrazp1233@gmail.com>
2023-08-18 10:04:56 +0300

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	_putchar("123456789");
+	_putchar("\n");
+}
