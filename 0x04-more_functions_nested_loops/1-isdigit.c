2926034ce266c694c4fb13865eda929c6f9d1d38
amrazp <amrazp1233@gmail.com>
2023-08-18 09:47:23 +0300

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,11 +6,11 @@
  * Description: define if a character is a number
  * @c: charater
  * Return: 1 yes 0 no
- */ 
+ */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'z')
+    if (c >= '0' && c <= '9')
 		return (1);
 	else 
 		return (0);
