772dcc3cc1092aa9899a16c26e3d8a2b0fb91cef
amrazp <amrazp1233@gmail.com>
2023-08-18 09:34:15 +0300

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  * Description: define if a character is a number
  * @c: charater
  * Return: 1 yes 0 no
- */ 
+ */
 
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'z')
-        return(1);
-    else 
-        return(0);
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
