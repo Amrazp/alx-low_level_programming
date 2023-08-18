f43474ef8595c11afeec003ff3067b0fd4d24f7f
amrazp <amrazp1233@gmail.com>
2023-08-18 10:32:35 +0300

    5

---
+++ 0x04-more_functions_nested_loops/ 5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+    int i , z;
+    for (i = 0; i < 15; i++)
+    {
+        for (z = 0; z < 15; z++)
+        {
+            _putchar('z');
+        }
+        _putchar('\n');
+    }
+    
+}
