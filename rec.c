cbdafd66b268e7eb90e1bb395019b8ae9e74677e
amrazp <amrazp1233@gmail.com>
2023-09-06 21:44:30 +0300

    00

--- rec.c
+++ rec.c
@@ -1,5 +1,28 @@
 #include "stdio.h"
-void main()
+
+
+   // Write a function that returns the length of a string.
+
+
+
+    int _strlen(char *s)
 {
+
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
     
+}
+
+
+int main()
+{
+    int _strlen(char *s);
 }
